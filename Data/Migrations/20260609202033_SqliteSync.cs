384c1082eac72e8ad677ad48b6080d55207641d2
Fatjeta Nerguti <fatjetanerguti8@gmail.com>
2026-06-09 22:20:53 +0200

    Add SqliteSync migration

---
+++ Data/Migrations/20260609202033_SqliteSync.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SqliteSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
